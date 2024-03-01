4f74729a59f944817f126f5ba373eceb45386004
sattar shakeel <154556813+sattar-shakeel@users.noreply.github.com>
2024-03-02 00:31:38 +0500

    Add files via upload

---
+++ Calculator/app.js
@@ -0,0 +1,27 @@
+
+
+// Calculator
+
+var num1 = 10;
+var num2 = 5;
+
+document.write("This is my Calculator", "<br/>")
+
+var sum = num1 + num2;
+document.write("sum of two number = " , sum , "<br/>")
+
+
+var sub = num1 - num2;
+document.write("Subtraction of two number = " , sub , "<br/>")
+
+
+var mul = num1 * num2;
+document.write("Multiplication of two num = " , mul , "<br/>" )
+
+
+var div = num1 / num2
+document.write("devide of two number = " , div , "<br/>")
+
+var reminder = num1 % num2;
+document.write("Reminder of two number = " , reminder , "<br/>")
+
